57d3e368233907f8e977fb46ebd75be0cf305b7f
AhmadAbdallah13 <a7mtoo133@gmail.com>
2024-12-20 21:39:25 +0300

    fix the usage stats plugin tracking

--- android/app/src/main/java/io/ionic/starter/AppUsageStats.java
+++ android/app/src/main/java/io/ionic/starter/AppUsageStats.java
@@ -1,12 +1,12 @@
 package io.ionic.starter;
 
+import android.app.usage.UsageEvents;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.provider.Settings;
 import android.app.AppOpsManager;
 
@@ -18,7 +18,10 @@ import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 @CapacitorPlugin(name = "AppUsageStats")
 public class AppUsageStats extends Plugin {
@@ -29,32 +32,63 @@ public class AppUsageStats extends Plugin {
       UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
 
       Calendar calendar = Calendar.getInstance();
-      long endTime = calendar.getTimeInMillis();
-      calendar.set(Calendar.HOUR_OF_DAY, 0);
+      calendar.setTimeZone(TimeZone.getTimeZone("Asia/Amman"));
+      long endTime = System.currentTimeMillis();
+      calendar.set(Calendar.HOUR_OF_DAY,0);
       calendar.set(Calendar.MINUTE, 0);
       calendar.set(Calendar.SECOND, 0);
+      calendar.set(Calendar.MILLISECOND,0);
       long startTime = calendar.getTimeInMillis();
 
       List<UsageStats> usageStatsList = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime);
 
       JSArray statsArray = new JSArray();
+
+      Map<String , Long> usageTimeOfApps = new HashMap<>();
+      Map<String , Long> prev = new HashMap<>();
+
+      UsageEvents usageEvents = usageStatsManager.queryEvents(startTime, endTime);
+      usageStatsManager.queryAndAggregateUsageStats(startTime, endTime);
+      while (usageEvents.hasNextEvent()) {
+        UsageEvents.Event event = new UsageEvents.Event();
+        usageEvents.getNextEvent(event);
+        String currPackageName=event.getPackageName();
+
+        if(!usageTimeOfApps.containsKey(currPackageName)){
+          usageTimeOfApps.put(currPackageName, 0L);
+        }
+        if(!prev.containsKey(currPackageName)){
+          prev.put(currPackageName, -1L);
+        }
+
+        if (event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+          prev.put(currPackageName, event.getTimeStamp());
+        } else if (event.getEventType() == UsageEvents.Event.MOVE_TO_BACKGROUND && prev.get(currPackageName) != -1) {
+          Long time = usageTimeOfApps.get(currPackageName) + (event.getTimeStamp() - prev.get(currPackageName));
+          usageTimeOfApps.put(currPackageName, time);
+        }
+      }
+
       PackageManager packageManager = context.getPackageManager();
 
-      for (UsageStats stats : usageStatsList) {
-          try {
-              ApplicationInfo appInfo = packageManager.getApplicationInfo(stats.getPackageName(), 0);
-              String appName = packageManager.getApplicationLabel(appInfo).toString();
+      for (Map.Entry<String, Long> entry : usageTimeOfApps.entrySet()) {
+        if(entry.getValue() > 0){
+          String packageName = entry.getKey();
+          String appName;
 
-              if (stats.getTotalTimeInForeground() > 0) {
-                  JSObject statsObject = new JSObject();
-                  statsObject.put("packageName", stats.getPackageName());
-                  statsObject.put("appName", appName);
-                  statsObject.put("totalTimeForeground", stats.getTotalTimeInForeground());
-                  statsArray.put(statsObject);
-              }
+          try {
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(packageName, 0);
+            appName = packageManager.getApplicationLabel(appInfo).toString();
           } catch (PackageManager.NameNotFoundException e) {
-              // Skip apps that can't be resolved
+            appName = packageName;
           }
+
+          JSObject statsObject = new JSObject();
+          statsObject.put("packageName", packageName);
+          statsObject.put("appName", appName);
+          statsObject.put("totalTimeForeground", entry.getValue());
+          statsArray.put(statsObject);
+        }
       }
 
     JSObject result = new JSObject();
